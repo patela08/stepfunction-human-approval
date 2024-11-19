2a89840bf874a93d4e027755af6197d3238fbd40
Akshar Patel <axrptl@Akshars-MacBook-Air.local>
2024-11-18 21:27:36 -0700

    Initial commit

---
+++ test/step-function-human-approval.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepFunctionHumanApproval from '../lib/step-function-human-approval-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step-function-human-approval-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepFunctionHumanApproval.StepFunctionHumanApprovalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
